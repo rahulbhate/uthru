dd23f23758bd154ef682f437b9a7508940cfe4e6
rahulbhate <bhate.rahul@gmail.com>
2020-07-27 11:52:09 +1000

    Media queries updated

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,20 +2,15 @@ import React, {useState, useEffect} from 'react';
 import '../styles/_menu.scss';
 import styles from "./header.module.scss"
 import { Link } from "gatsby"
-import { GiHamburgerMenu } from 'react-icons';
 const Sidebar = () =>{
-     const [open,setOpen] = useState(true);
+    const [open,setOpen] = useState(true);
     const listenClickEvent = event => {
-       setOpen(!open);
+    setOpen(!open);
     }
     return(
-        <>
-         <div className="menu-btn"><span className={open ? "menu-btn__burger" : "menu-btn__burger open"} onClick={listenClickEvent}></span><div className={open ? "menu-nav__open": "menu-nav"}><NavMenu /></div></div>
-         
-         </>
+        <div className="menu-btn"><span className={open ? "menu-btn__burger" : "menu-btn__burger open"} onClick={listenClickEvent}></span><div className={open ? "menu-nav__open": "menu-nav"}><NavMenu /></div></div>
     )
 }
-  
   const NavMenu = props => {
     return (
     <>
